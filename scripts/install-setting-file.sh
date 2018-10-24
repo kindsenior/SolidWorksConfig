f06fac4737d025592581538162dbc03932bbb7fb
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-10-24 23:15:49 +0900

    Fix for user name including white space

--- scripts/install-setting-file.sh
+++ scripts/install-setting-file.sh
@@ -1,30 +1,20 @@
-
 project_dir=$(cd $(dirname ${0}); pwd;)
 project_dir=$(cygpath -w ${project_dir/\/scripts/})
+## Since cygpath converted white space to unspecified symbol,
+## next command is neccessary to reconvert it to white space
+project_dir=$(echo ${project_dir})
 echo "project dir is "$project_dir
 project_dir_yen=${project_dir//\\/\\\\\\\\}
 echo "project dir with \ is "$project_dir_yen
 project_dir_slash=${project_dir//\\/\/}
 echo "project dir slash is "$project_dir_slash
 
-# generate users
-users=()
-usersdir=${USERPROFILE}\\..\\
-users+=`ls ${usersdir}|grep -v cyg|grep -vi public|grep -vi user|grep -vi default|grep -vi desktop`
-
-echo "user name list:"
-for user in $users
-do
-    echo " "$user
-done
-
-for user in $users
-do
-    sed $project_dir/settings/swSettings_general.sldreg -e s#@project_dir_slash@#${project_dir_slash}#gi \
-    | sed -e s#@project_dir_yen@#${project_dir_yen}#gi \
-    | sed -e s#@user_dir@#${drive}\:\\\\\\\\Users\\\\\\\\${user}#gi \
-    > $project_dir/settings/swSettings_${user}.sldreg
-done
-
-user=${users[0]}
+user=${USERPROFILE##*\\}
+user_no_space=${user/ }
+user=${user/ /\\ }
 echo "user name is "${user}
+
+sed "${project_dir}"/settings/swSettings_general.sldreg -e s#@project_dir_slash@#"${project_dir_slash}"#gi \
+| sed -e s#@project_dir_yen@#"${project_dir_yen}"#gi \
+| sed -e s#@user_dir@#"${drive}\:\\\\Users\\\\${user}"#gi \
+> "${project_dir}"/settings/swSettings_${user_no_space}.sldreg
