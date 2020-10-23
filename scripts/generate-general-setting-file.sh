141f2a3766da86c2a783e00c649a6aaf290d7fa6
kindsenior <kunio.einstein@gmail.com>
2020-10-23 22:47:38 +0900

    [scripts] support wsl

--- scripts/generate-general-setting-file.sh
+++ scripts/generate-general-setting-file.sh
@@ -1,24 +1,32 @@
+if [ -n "$WSL_DISTRO_NAME" ]; then
+    echo wsl mode
+    IS_WSL=1
+else
+    echo windows mode
+fi
 
-project_dir=$(cd $(dirname ${0}); pwd;)
-project_dir=$(cygpath -w ${project_dir/\/scripts/})
+apparent_project_dir=$(cd $(dirname ${0}); pwd;)
+apparent_project_dir=${apparent_project_dir/\/scripts/}
+if [ $IS_WSL ];then
+    project_dir=$(wslpath -w $(readlink -f ${apparent_project_dir}))
+else
+    project_dir=$(cygpath -w ${apparent_project_dir})
+fi
+
+echo "apparent project dir is "$apparent_project_dir
 echo "project dir is "$project_dir
 
-# generate users
-users=()
-usersdir=${USERPROFILE}\\..\\
-users+=`ls ${usersdir}|grep -v cyg|grep -vi public|grep -vi user|grep -vi default|grep -vi desktop`
-
-echo "user name is"
-for user in $users
-do
-    echo " "$user
-done
-
-user=${users[0]}
+if [ $IS_WSL ]; then
+    user=$(/mnt/c/Windows/System32/cmd.exe /c echo %USERPROFILE% | nkf -Lu | sed -e 's#^.*\\##')
+else
+    user=${USERPROFILE##*\\}
+fi
+user_no_space=${user/ }
+user=${user/ /\\ }
 echo "user name is "${user}
 
 # input file name is swSettings.sldreg
-sed $project_dir/settings/swSettings.sldreg -e s#[^\"\;]*/solidworksconfig#@project_dir_slash@#gi \
+sed ${apparent_project_dir}/settings/swSettings.sldreg -e s#[^\"\;]*/solidworksconfig#@project_dir_slash@#gi \
     | sed  -e s#[^\"\;]*\\\\solidworksconfig#@project_dir_yen@#gi \
     | sed  -e s#[^\"\;]*\\\\${user}#@user_dir@#gi \
-    > $project_dir/settings/swSettings_general.sldreg
+    > ${apparent_project_dir}/settings/swSettings_general.sldreg
--- scripts/install-setting-file.sh
+++ scripts/install-setting-file.sh
@@ -1,7 +1,20 @@
-project_dir=$(cd $(dirname ${0}); pwd;)
-project_dir=$(cygpath -w ${project_dir/\/scripts/})
+if [ -n "$WSL_DISTRO_NAME" ]; then
+    echo wsl mode
+    IS_WSL=1
+else
+    echo windows mode
+fi
+
+apparent_project_dir=$(cd $(dirname ${0}); pwd;)
+apparent_project_dir=${apparent_project_dir/\/scripts/}
+if [ $IS_WSL ];then
+    project_dir=$(wslpath -w $(readlink -f ${apparent_project_dir}))
+else
+    project_dir=$(cygpath -w ${apparent_project_dir})
+fi
 ## Since cygpath converted white space to unspecified symbol,
 ## next command is neccessary to reconvert it to white space
+echo "apparent project dir is "$apparent_project_dir
 project_dir=$(echo ${project_dir})
 echo "project dir is "$project_dir
 project_dir_yen=${project_dir//\\/\\\\\\\\}
@@ -9,12 +22,16 @@ echo "project dir with \ is "$project_dir_yen
 project_dir_slash=${project_dir//\\/\/}
 echo "project dir slash is "$project_dir_slash
 
-user=${USERPROFILE##*\\}
+if [ $IS_WSL ]; then
+    user=$(/mnt/c/Windows/System32/cmd.exe /c echo %USERPROFILE% | nkf -Lu | sed -e 's#^.*\\##')
+else
+    user=${USERPROFILE##*\\}
+fi
 user_no_space=${user/ }
 user=${user/ /\\ }
 echo "user name is "${user}
 
-sed "${project_dir}"/settings/swSettings_general.sldreg -e s#@project_dir_slash@#"${project_dir_slash}"#gi \
+sed "${apparent_project_dir}"/settings/swSettings_general.sldreg -e s#@project_dir_slash@#"${project_dir_slash}"#gi \
 | sed -e s#@project_dir_yen@#"${project_dir_yen}"#gi \
 | sed -e s#@user_dir@#"${drive}\:\\\\Users\\\\${user}"#gi \
-> "${project_dir}"/settings/swSettings_${user_no_space}.sldreg
+> "${apparent_project_dir}"/settings/swSettings_${user_no_space}.sldreg
